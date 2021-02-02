0de5a0443c4d9dbc04962920ba477b93e629fdb7
tej448 <anithalingathoti12@gmail.com>
2021-02-02 15:24:44 +0530

    third commit

---
+++ str.py
@@ -0,0 +1,84 @@
+a=' '
+print(type(a))
+
+a="welcome to python lab"
+print(a)
+
+# concatination of string
+b='workshop'
+print(a+b)
+
+# getting values
+print(a[0])
+print(a[1])
+print(a[2])
+
+# forward index and backward index
+# forward index
+print(a[0])
+print(a[5])
+
+# backword index
+print(a[-1])
+print(a[-5])
+  
+s= "welcome to python"
+print(s[-1])
+print(s[-5])
+
+# slicing operation-->:
+name='python programming'
+# print(name[:])
+# print(name[0:6])
+# print(name[4:6])
+# print(name[::-1])
+# print(name[0:17:2])
+print(name[6:7])
+print(name[0:])
+print(name[0:1])
+
+
+# string methods in 3 ways
+# by using dsta type------>str
+# print(dir(str))
+# n=''
+# print(dir(n))
+
+# m= 'hello'
+# print(dir(m))
+p='welcome'
+# print(p.capitalize())
+
+# print(p.casefold())
+# print(p.swapcase())
+# print(p.count('S'))
+# print(p.index('z'))
+# print(p.find('e'))
+# print(p.find('x'))
+# print(p.center(20,'*'))
+# print('My name is {0} & i am from {1}'.format('ani','akp'))
+# print(p.upper())
+# print(p.lower())
+# print(p.title())
+# print(p.isupper())
+# print(p.islower())
+# print(p.startswitch('w'))
+# print(p.endswitch('c'))
+# print(p.istitle())
+q='<---'
+# print(q.join(p))
+# print(p.isdigit())
+# print(p.isalpha())
+# print(p.isalnum())
+# print('python\tprogramming\tworkshop'.expandtabs())
+'''split,
+splitlines,
+strip,
+Istrip,
+rstrip'''
+print(p.split())
+print(p.strip())
+print(p.lstrip())
+print(p.rstrip())
+
+
